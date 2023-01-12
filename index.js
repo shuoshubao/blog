deed3201cfe0b9262d8fda789600db3bff90a049
shuoshubao <759979885@qq.com>
2023-01-12 21:44:08 +0800

    feat: 变更数据结构; 增加文件的 size, ctime, mtime

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const { readFileSync, writeFileSync } = require('fs')
+const { readFileSync, writeFileSync, lstatSync } = require('fs')
 const { resolve } = require('path')
 
 const data = JSON.parse(readFileSync('./data/db.json').toString())
 
+const CategoryData = JSON.parse(JSON.stringify(data))
+const ContentData = {}
+
 Object.entries(data).forEach(([k, v]) => {
-  v.forEach(v2 => {
-    const filepath = resolve(__dirname, 'article', k, `${v2.name}.md`)
+  v.forEach((v2, i2) => {
+    const { name, title } = v2
+    const filepath = resolve(__dirname, 'article', k, `${name}.md`)
     const content = readFileSync(filepath).toString()
-    v2.content = content
+    const { size, ctime, mtime } = lstatSync(filepath)
+    ContentData[[k, name].join('/')] = {
+      title,
+      content
+    }
+    Object.assign(CategoryData[k][i2], { content, size, ctime, mtime })
   })
 })
 
-writeFileSync('./store/all.json', JSON.stringify(data, ' ', 2) + '\n')
+writeFileSync('./store/data.json', JSON.stringify(CategoryData, ' ', 2) + '\n')
+writeFileSync('./store/all.json', JSON.stringify(ContentData, ' ', 2) + '\n')
